14dc7dd68f09479b402887028af0a7ecc83030c1
medialab <medialab@spongnet.uk>
2025-06-02 09:59:23 +0100

    types

--- app/models/event_types.py
+++ app/models/event_types.py
@@ -4,13 +4,15 @@ class EventType(str, Enum):
     NONE = "None"
     NOTIFY = "Notify"
     BACKUP = "Backup"
+    TASK = "Task"
 
 class SubEventType(str, Enum):
     NONE = "None"
     EMAIL = "Email"
     NTFY = "Ntfy"
     SNAPRAID = "Snapraid"
-
+    TASK_RUN = "Task Run"
+    
 class Status(str, Enum):
     """Status enum for event status values."""
     INFO = "Info"
